41c7252ae3240f9d302cefe7146f76b172cabacb
Sebastiano <sebastiano.gaudeano@live.com>
2017-03-31 15:42:13 +0200

    Add A View to Insert no Movie

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,7 +1,9 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -34,6 +36,26 @@ namespace Vidly.Controllers
                 .Movies
                 .Include( g => g.Genre )
                 .SingleOrDefault( m => m.Id == id ) );
+        }
+
+        public ActionResult New ()
+        {
+            MovieFormViewModel viewModel = new MovieFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View( "MovieForm" , viewModel );
+        }
+
+        [HttpPost]
+        public ActionResult Save ( Movie movie )
+        {
+            movie.AddedDate = DateTime.Now;
+            _context.Movies.Add( movie );
+            _context.SaveChanges();
+
+            return RedirectToAction( "Index" , "Movies" );
         }
     }
 }
