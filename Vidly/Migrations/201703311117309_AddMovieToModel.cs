dc5bbe95dd98e25d2f76f33025e5e7f1e06d78f6
Sebastiano <sebastiano.gaudeano@live.com>
2017-03-31 13:20:38 +0200

    Add Movie to model and add migration to add it to database. Add Genre Class

---
+++ Vidly/Migrations/201703311117309_AddMovieToModel.cs
@@ -0,0 +1,30 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMovieToModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        AddedDate = c.DateTime(nullable: false),
+                        ReleaseDate = c.DateTime(nullable: false),
+                        NumberInStock = c.Byte(nullable: false),
+                        NumberAvailable = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+        }
+    }
+}
---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public byte NumberInStock { get; set; }
+        public byte NumberAvailable { get; set; }
+    }
+}
