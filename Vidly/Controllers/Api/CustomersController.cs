2586361011a11a2c61ba35c74e658cee75698363
Sebastiano <sebastiano.gaudeano@live.com>
2017-04-04 02:45:31 +0200

    Add a Web Api Controller and add GetCustomers and GetCustomer method to retrieve Customer whitin Api

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController ()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<Customer> GetCustomers () => _context.Customers.ToList();
+
+
+        public IHttpActionResult GetCustomer ( int id )
+        {
+            Customer customer = _context.Customers.SingleOrDefault( c => c.Id == id );
+
+            if ( customer == null )
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+
+
+    }
+}
