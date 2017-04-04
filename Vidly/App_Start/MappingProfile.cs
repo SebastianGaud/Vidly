4b6e32be74e387d4185d0b42e7cf1970151cb077
Sebastiano <sebastiano.gaudeano@live.com>
2017-04-04 15:38:17 +0200

    Add Ingore to field Id, to avoid Exception

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -8,9 +8,15 @@ namespace Vidly
     {
         public MappingProfile ()
         {
-            Mapper.CreateMap<Customer , CustomerDTO>();
+            Mapper.CreateMap<Customer , CustomerDTO>()
+                .ForMember( c => c.Id , opt => opt.Ignore() );
+
             Mapper.CreateMap<CustomerDTO , Customer>();
-            Mapper.CreateMap<Movie , MovieDTO>();
+
+
+            Mapper.CreateMap<Movie , MovieDTO>()
+                .ForMember( m => m.Id , opt => opt.Ignore() );
+
             Mapper.CreateMap<MovieDTO , Movie>();
         }
     }
